b032790e4fd9cdbc5f52b78f7d9065b5413da0be
Artem Shashov <tauquantum@gmail.com>
2023-04-21 18:12:55 +1000

    added stocks_products

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            values_for_update = position
+            defaults = {'price': values_for_update.get('price'), 'quantity': values_for_update.get('quantity')}
+            stockProduct, created = StockProduct.objects.update_or_create(stock_id=stock.id,
+                                                                          product_id=values_for_update.get(
+                                                                              'product').id,
+                                                                          defaults=defaults)
+
+        return stock
